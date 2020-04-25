42cbc7b2e94bf2f435b61ab1ef3cd15048459a26
Irvan Naufali Rahmanto <42972909+irvanrahmanto@users.noreply.github.com>
2020-04-26 00:14:09 +0700

    menambahkan konfigurasi

--- src/index.js
+++ src/index.js
@@ -16,8 +16,27 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 // Error handling
-io.on('connection', () => {
+io.on('connection', (socket) => {
     console.log('Success create socket connection');
+
+    // If user succes joined 
+    socket.emit('message', 'welcome!');
+    socket.broadcast.emit('message', 'A new user has been joined!');
+
+    // sending message
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message);
+    });
+
+    // sending current location, get latitude and longitude from geolocation api and sending that api to google maps
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    })
+
+    // If user left joined
+    socket.on('disconnect', () => {
+        io.emit('message', 'a user has left!');
+    })
 });
 
 server.listen(PORT, () => {
