3316d7dcccdd99d1db91afd2edc961255e51d986
Irvan Naufali Rahmanto <42972909+irvanrahmanto@users.noreply.github.com>
2020-04-25 17:46:13 +0700

    memperbarui konfigurasi

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 // inisialisasi depedency
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // penggunaan depedency
 const PORT = process.env.PORT || 3000;
@@ -12,6 +16,10 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 
 // Error handling
-app.listen(PORT, () => {
+io.on('connection', () => {
+    console.log('Success create socket connection');
+});
+
+server.listen(PORT, () => {
     console.log("Server running at " + PORT + "!!");
 });
